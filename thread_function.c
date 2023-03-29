ed2d591cedabf32025ae02ed97084af42bb0f105
Lin <lsun@student.hive.fi>
2023-03-29 12:27:34 +0300

    start eating

--- thread_function.c
+++ thread_function.c
@@ -11,13 +11,34 @@ void* philo_needs_to_eat(void *arg)
 	gettimeofday(&before, NULL);
 	if (ph->thread_id % 2 == 0)
 	{
-		printf("%lu philo %d and I can eat first.\n", timestamp(before), ph->thread_id);
+		while (ph->meal_count < ph->must_eat)
+		{
+			pthread_mutex_lock(&ph->left);
+			printf("%lu philo %d took a left fork.\n", timestamp(before), ph->thread_id);
+			pthread_mutex_lock(&ph->right);
+			printf("%lu philo %d took a right fork.\n", timestamp(before), ph->thread_id);
+			printf("%lu philo %d is eating.\n", timestamp(before), ph->thread_id);
+			usleep(ph->time_to_eat);
+			pthread_mutex_unlock(&ph->right);
+			pthread_mutex_unlock(&ph->left);
+			if (ph->must_eat != -1)
+				ph->meal_count++;
+		}
 	}
 	else
 	{
-		usleep(1000000);
-		//ft_usleep(1000000);
-		printf("%lu philo %d and I will wait first.\n", timestamp(before), ph->thread_id);
+		while (ph->meal_count < ph->must_eat)
+		{
+			usleep(ph->time_to_eat/2);
+			pthread_mutex_lock(&ph->right);
+			printf("%lu philo %d took a left fork.\n", timestamp(before), ph->thread_id);
+			pthread_mutex_lock(&ph->left);
+			printf("%lu philo %d is eating.\n", timestamp(before), ph->thread_id);
+			pthread_mutex_unlock(&ph->left);
+			pthread_mutex_unlock(&ph->right);
+			if (ph->must_eat != -1)
+				ph->meal_count++;
+		}
 	}
 	return (NULL);
 }
