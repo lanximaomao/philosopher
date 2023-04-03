3b65078609025c1608d194bd7d3983800b3ad386
Lin <lsun@student.hive.fi>
2023-04-03 11:40:25 +0300

    fix a few bugs

--- thread_function.c
+++ thread_function.c
@@ -12,14 +12,12 @@ void* philo_needs_to_eat(void *arg)
 	ph = (t_philo*)arg;
 
 	if (ph->thread_id % 2 != 0)
-		usleep(150);
+		usleep(50);
 	while (ph->meal_count == -1 || ph->meal_count < ph->must_eat)
 	{
 		//printf("start = %llu && ph->last_meal = %llu\n", ph->start, ph->last_meal);
 		if (ph->is_alive != 1)
-		{
 			break;
-		}
 		//if (timestamp(ph->last_meal) >= ph->time_to_die)
 		//{
 		//	ph->is_alive = 0;
@@ -32,17 +30,25 @@ void* philo_needs_to_eat(void *arg)
 		pthread_mutex_lock(ph->mutex_right);
 		//*(ph->fork_right) = ph->thread_id;
 		printf("%llu %d has taken a fork\n", timestamp(ph->start), ph->thread_id);
-		printf("%llu %d is eating\n", timestamp(ph->start), ph->thread_id);
-		//ft_usleep(ph->time_to_eat);
-		ph->previous_meal = ph->last_meal;
-		ph->last_meal = get_current_time();
-		usleep(ph->time_to_eat * 1000);
+		if (ph->is_alive == 1)
+		{
+			printf("%llu %d is eating\n", timestamp(ph->start), ph->thread_id);
+			//ft_usleep(ph->time_to_eat);
+			ph->previous_meal = ph->last_meal;
+			ph->last_meal = get_current_time();
+			usleep(ph->time_to_eat * 1000);
+		}
+		else
+			return(NULL);
 		//*(ph->fork_right) = 0;
 		//*(ph->fork_left) = 0;
 		pthread_mutex_unlock(ph->mutex_right);
 		pthread_mutex_unlock(ph->mutex_left);
-		printf("%llu %d is sleeping\n", timestamp(ph->start), ph->thread_id);
+
 		//ft_usleep(ph->time_to_sleep);
+		if (ph->is_alive != 1)
+			break;
+		printf("%llu %d is sleeping\n", timestamp(ph->start), ph->thread_id);
 		usleep(ph->time_to_sleep * 1000);
 		if (ph->must_eat != -1)
 			ph->meal_count++;
