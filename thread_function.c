0de3ad4b048206d6171c7cae43db7e4d59e68d91
Lin <lsun@student.hive.fi>
2023-04-08 14:26:56 +0300

    working for 4/500/200/200/200

--- thread_function.c
+++ thread_function.c
@@ -1,57 +1,42 @@
-
 #include "philo.h"
-
 
 void* philo_needs_to_eat(void *arg)
 {
 	int i;
 	t_philo *ph;
 
-
 	i = 0;
 	ph = (t_philo*)arg;
-
 	if (ph->thread_id % 2 != 0)
-		usleep(50);
+		usleep(ph->time_to_eat);
 	while (ph->meal_count == -1 || ph->meal_count < ph->must_eat)
 	{
-		//printf("start = %llu && ph->last_meal = %llu\n", ph->start, ph->last_meal);
 		if (ph->is_alive != 1)
 			break;
-		//if (timestamp(ph->last_meal) >= ph->time_to_die)
-		//{
-		//	ph->is_alive = 0;
-		//	break;
-		//}
 		printf("%llu %d is thinking\n", timestamp(ph->start), ph->thread_id);
 		pthread_mutex_lock(ph->mutex_left);
-		//*(ph->fork_left) = ph->thread_id;
 		printf("%llu %d has taken a fork\n", timestamp(ph->start), ph->thread_id);
 		pthread_mutex_lock(ph->mutex_right);
-		//*(ph->fork_right) = ph->thread_id;
 		printf("%llu %d has taken a fork\n", timestamp(ph->start), ph->thread_id);
 		if (ph->is_alive == 1)
 		{
 			printf("%llu %d is eating\n", timestamp(ph->start), ph->thread_id);
-			//ft_usleep(ph->time_to_eat);
 			ph->previous_meal = ph->last_meal;
 			ph->last_meal = get_current_time();
 			usleep(ph->time_to_eat * 1000);
 		}
 		else
 			return(NULL);
-		//*(ph->fork_right) = 0;
-		//*(ph->fork_left) = 0;
 		pthread_mutex_unlock(ph->mutex_right);
 		pthread_mutex_unlock(ph->mutex_left);
-
-		//ft_usleep(ph->time_to_sleep);
 		if (ph->is_alive != 1)
 			break;
 		printf("%llu %d is sleeping\n", timestamp(ph->start), ph->thread_id);
 		usleep(ph->time_to_sleep * 1000);
 		if (ph->must_eat != -1)
 			ph->meal_count++;
+		if (ph->meal_count == ph->must_eat)
+			break;
 	}
 	return (NULL);
 }
@@ -65,6 +50,7 @@ void* vital_monitor(void *arg)
 {
 	int i;
 	int j;
+	int count;
 	t_philo *ph;
 
 	i = 0;
@@ -77,25 +63,24 @@ void* vital_monitor(void *arg)
 		if (ph[i].last_meal - ph[i].previous_meal >= ph[i].time_to_die)
 		{
 			ph->is_alive = 0;
-			//printf("\n\n\n%llu, %llu, %d\n", ph[i].last_meal, ph[i].start, ph[i].time_to_die);
 			break;
+		}
+		if (ph[i].meal_count == ph[i].must_eat)
+		{
+			count++;
+			if (count == ph[i].num)
+				break;
 		}
 		i++;
 		if (i >= ph[0].num)
 			i = 0;
-		//printf("i = %d\n", i);
 	}
 	while (j < ph[0].num)
 	{
-		//if ( j != i)
 			ph[j].is_alive = 0;
 		j++;
 	}
-	//printf("i = %d\n", i);
 	death_announcement(timestamp(ph[i].start), ph[i].thread_id);
-	//printf("hello from monitor.\n");
-	//printf("philo %d\n status %d\n", ph[1].thread_id, ph[1].status);
-	//death_announcement(timestamp(start), ph->thread_id);
 	return(NULL);
 }
 
